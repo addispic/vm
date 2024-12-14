4b9af34436cec092d9cfbdcc214299554d079fbe
addispic <addispik@gmail.com>
2024-12-14 14:00:56 +0300

    changes

--- client/src/config.jsx
+++ client/src/config.jsx
@@ -1,6 +1,6 @@
 import {io} from 'socket.io-client'
 // base url
-export const BASE_URI = "http://localhost:5050";
+export const BASE_URI = "https://vm-backend-8e3r.onrender.com";
 
 // socket
-export const SOCKET = io('ws://localhost:5050');
+export const SOCKET = io('ws://vm-backend-8e3r.onrender.com');
--- server/index.js
+++ server/index.js
@@ -12,6 +12,17 @@ const dbConnectionHandler = require("./api/db/db.connection");
 // port
 const PORT = process.env.PORT || 5050;
 
+// cors
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // settings
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
