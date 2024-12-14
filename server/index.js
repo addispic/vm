344eb1e7d7285d0500cd0e6e7f74bc9699560bda
addispic <addispik@gmail.com>
2024-12-14 14:10:36 +0300

    changes

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5050;
 
 // cors
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://vm-frontend-3l1x.onrender.com");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: true,
+    origin: "https://vm-frontend-3l1x.onrender.com",
     credentials: true,
   })
 );
