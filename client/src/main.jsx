70b9475349ef9a5c1584e9cf32af11cca30fd8b0
addispic <addispik@gmail.com>
2024-12-14 14:30:47 +0300

    dist deleted

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,20 @@
 import { createRoot } from "react-dom/client";
-import {BrowserRouter} from 'react-router-dom'
-import {Provider} from 'react-redux';
-import axios from 'axios'
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import axios from "axios";
 
-import {store} from './app/store.js'
+import { store } from "./app/store.js";
 import App from "./App.jsx";
 import "./index.css";
 
 // axios settings
-axios.defaults.baseURL = "http://localhost:5050";
-axios.defaults.withCredentials = true
+axios.defaults.baseURL = "https://vm-backend-8e3r.onrender.com";
+axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
+  <BrowserRouter>
     <Provider store={store}>
-        <App />
+      <App />
     </Provider>
-    </BrowserRouter>
+  </BrowserRouter>
 );
