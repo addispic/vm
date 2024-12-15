1c99235abaf014549f25806547a1fe8a5fc1c80d
addispic <addispik@gmail.com>
2024-12-15 04:24:39 +0300

    frontend is done abit

--- client/src/config.jsx
+++ client/src/config.jsx
@@ -1,6 +1,9 @@
 import { io } from "socket.io-client";
 // base url
-export const BASE_URI = "http://localhost:5050";
+// export const BASE_URI = "http://localhost:5050";
+export const BASE_URI = "https://vm-server-9sgr.onrender.com";
 
 // socket
-export const SOCKET = io("ws://localhost:5050");
+// export const SOCKET = io("ws://localhost:5050");
+// export const SOCKET = io("https://vm-server-9sgr.onrender.com");
+export const SOCKET = io("https://vm-server-9sgr.onrender.com");
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,8 +8,8 @@ import App from "./App.jsx";
 import "./index.css";
 
 // axios settings
-axios.defaults.baseURL = "http://localhost:5050";
-// axios.defaults.baseURL = "https://vm-backend-8e3r.onrender.com";
+// axios.defaults.baseURL = "http://localhost:5050";
+axios.defaults.baseURL = "https://vm-server-9sgr.onrender.com";
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")).render(
